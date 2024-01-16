82cfea4f2748cd3e42ca1def743c2bae8b21bad0
Braden MacDonald <mail@bradenm.com>
2024-01-16 08:48:53 -0800

    Fix issue with async map desctruction happening after 2nd construction (in dev)

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import * as maplibregl from "maplibre-gl";
+import * as pmtiles from "pmtiles";
 
 import { layers, mapSource } from "./basemap-layers.ts";
 import { MapContext } from "./MapUtils.ts";
@@ -18,65 +20,62 @@ export const Map: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [map, setMap] = React.useState<maplibregl.Map>();
 
     React.useEffect(() => {
-        const mapPromise = (async function () {
-            // Load the map dependencies asynchronously via a separate bundle:
-            const maplibregl = await import("maplibre-gl");
-            if (needPmTiles && !pmTilesInitialized) {
-                // Load the "pmtiles" protocol that allows us to serve all the vector tiles for the map from a single large static .pmtiles file.
-                const pmtiles = await import("pmtiles"); // Only import if we need to; not required in production when using a CDN
-                const protocol = new pmtiles.Protocol();
-                maplibregl.default.addProtocol("pmtiles", protocol.tile);
-                pmTilesInitialized = true;
-            }
-            const initialUrl = new URL(location.href);
-            const initialZoom = constrain(parseFloat(initialUrl.searchParams.get("z") ?? ""), 5, 20, 10);
-            const initialLng = constrain(parseFloat(initialUrl.searchParams.get("lng") ?? ""), -140, -115, -122.94536684722912);
-            const initialLat = constrain(parseFloat(initialUrl.searchParams.get("lat") ?? ""), 47, 60, 49.241584389313424);
+        // Load the map dependencies asynchronously via a separate bundle:
+        if (needPmTiles && !pmTilesInitialized) {
+            // Load the "pmtiles" protocol that allows us to serve all the vector tiles for the map from a single large static .pmtiles file.
+            const protocol = new pmtiles.Protocol();
+            maplibregl.default.addProtocol("pmtiles", protocol.tile);
+            pmTilesInitialized = true;
+        }
+        const initialUrl = new URL(location.href);
+        const initialZoom = constrain(parseFloat(initialUrl.searchParams.get("z") ?? ""), 5, 20, 10);
+        const initialLng = constrain(parseFloat(initialUrl.searchParams.get("lng") ?? ""), -140, -115, -122.94536684722912);
+        const initialLat = constrain(parseFloat(initialUrl.searchParams.get("lat") ?? ""), 47, 60, 49.241584389313424);
 
-            const map = new maplibregl.Map({
-                container: 'map',
-                zoom: initialZoom,
-                center: [initialLng, initialLat], // starting position [lng, lat]
-                style: {
-                    version: 8,
-                    name: "Transitopia",
-                    glyphs: "https://fonts.openmaptiles.org/{fontstack}/{range}.pbf",
-                    sprite: "https://openmaptiles.github.io/positron-gl-style/sprite",
-                    sources: {
-                        [mapSource]: {
-                            type: "vector",
-                            url: sourceUrlBase,
-                            attribution: 'Map: <a href="https://openstreetmap.org">OpenStreetMap</a> + <a href="https://openmaptiles.org/">OpenMapTiles </a> + <a href="https://maplibre.org/">MapLibre</a>'
-                        },
-                        "transitopia-cycling": {
-                            type: "vector",
-                            url: sourceUrlCycling,
-                            attribution: "",
-                        }
+        const map = new maplibregl.Map({
+            container: 'map',
+            zoom: initialZoom,
+            center: [initialLng, initialLat], // starting position [lng, lat]
+            style: {
+                version: 8,
+                name: "Transitopia",
+                glyphs: "https://fonts.openmaptiles.org/{fontstack}/{range}.pbf",
+                sprite: "https://openmaptiles.github.io/positron-gl-style/sprite",
+                sources: {
+                    [mapSource]: {
+                        type: "vector",
+                        url: sourceUrlBase,
+                        attribution: 'Map: <a href="https://openstreetmap.org">OpenStreetMap</a> + <a href="https://openmaptiles.org/">OpenMapTiles </a> + <a href="https://maplibre.org/">MapLibre</a>'
                     },
-                    layers,
-                }
-            });
-            map.getCanvas().style.cursor = 'default';
+                    "transitopia-cycling": {
+                        type: "vector",
+                        url: sourceUrlCycling,
+                        attribution: "",
+                    }
+                },
+                layers,
+            }
+        });
+        map.getCanvas().style.cursor = 'default';
 
-            const handleMapViewChanged = (() => {
-                const zoom = map.getZoom();
-                const { lng, lat } = map.getCenter();
-                const url = new URL(location.href);
-                url.searchParams.set("z", zoom.toFixed(4));
-                url.searchParams.set("lat", lat.toFixed(10));
-                url.searchParams.set("lng", lng.toFixed(10));
-                history.replaceState({ zoom, lng, lat }, "", url.pathname + url.search);
-            });
+        const handleMapViewChanged = (() => {
+            const zoom = map.getZoom();
+            const { lng, lat } = map.getCenter();
+            const url = new URL(location.href);
+            url.searchParams.set("z", zoom.toFixed(4));
+            url.searchParams.set("lat", lat.toFixed(10));
+            url.searchParams.set("lng", lng.toFixed(10));
+            history.replaceState({ zoom, lng, lat }, "", url.pathname + url.search);
+        });
 
-            map.on("zoomend", handleMapViewChanged);
-            map.on("moveend", handleMapViewChanged);
-            map.on("load", () => { setMap(map); });
-            return map;
-        })();
+        map.on("zoomend", handleMapViewChanged);
+        map.on("moveend", handleMapViewChanged);
+        map.on("load", () => { setMap(map); });
+
         // Cleanup:
         return () => {
-            mapPromise.then(map => map.remove());
+            console.log("Destroying map");
+            map.remove();
         }
     }, []);
 
