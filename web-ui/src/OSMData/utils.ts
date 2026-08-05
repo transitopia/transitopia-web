b7fe91104d0db2d3c777d5a792f686d0decf486a
Braden MacDonald <mail@bradenm.com>
2026-08-04 21:38:41 -0700

    chore(deps): bump all /web-ui deps

--- web-ui/src/OSMData/utils.ts
+++ web-ui/src/OSMData/utils.ts
@@ -5,11 +5,13 @@
 export function removeUndefinedKeys<Shape extends Record<string, unknown>>(
   obj: Shape,
 ): {
-  [K in keyof Shape as undefined extends Shape[K] ?
-    Shape[K] extends undefined ?
-      never
+  [
+    K in keyof Shape as undefined extends Shape[K] ?
+      Shape[K] extends undefined ?
+        never
+      : K
     : K
-  : K]: Exclude<Shape[K], undefined>;
+  ]: Exclude<Shape[K], undefined>;
 } {
   return Object.fromEntries(
     Object.entries(obj).filter(([_k, v]) => v !== undefined),
